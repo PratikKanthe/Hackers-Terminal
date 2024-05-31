f502da6632cc4f0cfc8a40267e943dceb1f156aa
Pratik Kanthe <115777270+PratikKanthe@users.noreply.github.com>
2024-05-31 17:33:27 +0530

    updated logic and animation

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-let container = document.getElementById("container");
+const container = document.getElementById("container");
 
-async function start() {
-  let sc = await document.createElement("p");
-  sc.innerHTML = "Start";
+async function createElem(val) {
+  let sc = document.createElement("p");
+  sc.innerHTML = val;
   container.appendChild(sc);
 }
-start();
+
+async function setData() {
+  setTimeout(() => {
+    createElem("Initializing Hacking<span class='dots'></span>");
+  }, 0);
+  setTimeout(() => {
+    createElem("Reading your Files<span class='dots'></span>");
+  }, 3000);
+  setTimeout(() => {
+    createElem("Password files Detected<span class='dots'></span>");
+  }, 6000);
+  setTimeout(() => {
+    createElem(
+      "Sending all passwords and personal files to server<span class='dots'></span>"
+    );
+  }, 9000);
+  setTimeout(() => {
+    createElem("Cleaning up<span class='dots'></span>");
+  }, 12000);
+}
+
+setData();
