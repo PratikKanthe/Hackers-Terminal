00f42aa095c6963e89d5a623f17c68b675a106c7
Pratik Kanthe <115777270+PratikKanthe@users.noreply.github.com>
2024-06-03 10:12:44 +0530

    updated data in one object

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const createElem = async (val) => {
   await randomDelay();
   let div = document.createElement("div");
   div.innerHTML = val;
-  document.body.append(container);
+  container.appendChild(div);
 };
 
 const randomDelay = () => {
-  return new Promise((resolve, reject) => {
-    timeout = 1 + 6 * Math.random();
+  return new Promise((resolve) => {
+    let timeout = 1 + 6 * Math.random();
     setTimeout(() => {
       resolve();
     }, timeout * 1000);
@@ -18,7 +18,7 @@ const randomDelay = () => {
 
 async function main() {
   let time = setInterval(() => {
-    let last = document.body.getElementsByTagName("div");
+    let last = container.getElementsByTagName("div");
     last = last[last.length - 1];
     if (last.innerHTML.endsWith("...")) {
       last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length - 3);
@@ -42,6 +42,5 @@ async function main() {
   await randomDelay();
   clearInterval(time);
 }
-main();
 
-setData();
+main();
